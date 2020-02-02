2f443791eb85079cabe0075ae51c59908f523147
Garubieru <gabrieldeusdeth@gmail.com>
2020-02-02 19:45:53 -0300

    Finished 'count_fasta' page. 'index' and 'gb2fa' created

--- fasta_count.js
+++ fasta_count.js
@@ -2,30 +2,52 @@
     var lines = content.split('\n')
     console.log(lines);
     var count = 0;
-    var header = '';
+    var seqlen = [];
+    var header = [];
     for (var i = 0; i < lines.length; i++) {
       if ( lines[i].startsWith('>') ) {
-        header = lines[i].slice(1) + ":"
+        if ( header.length == 0 ) {
+        header.push(lines[i].slice(1))
         continue
+        }
+        else {
+          header.push(lines[i].slice(1))
+          seqlen.push(count)
+          count = 0
+          continue
+        }
       }
       else {
         count += lines[i].length
+        console.log(count)
       }
     }
-      document.getElementById("seqlen").innerHTML = `${header} ${count} bp` 
+    seqlen.push(count)
+    console.log(header)
+    console.log(seqlen)
+    printSeqs(header, seqlen)
+  }
+
+
+  function printSeqs(header, seqlen) {
+    var myseqs = ''
+    for ( let i = 0; i < header.length; i++) {
+      myseqs += `${header[i]}\t${seqlen[i]} bp\n`
     }
+    console.log(myseqs)
+    document.getElementById("seqlen").innerHTML = myseqs.trim();
+  }
+  
 
   function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
-
     for (var i = 0, f; f = files[i]; i++) {
       console.log(f)
       var reader = new FileReader();
       reader.readAsText(f);
       reader.onload = function () {
         var content = reader.result;
-        document.getElementById("content").innerHTML = content;
-        countFasta(content)
+        countFasta(content);
       }
     }
   }
